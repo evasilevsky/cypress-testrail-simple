1c8a9d83eefec0bc820310533673a049a79e81d8
Eric Vasilevsky <evasilevsky@gmail.com>
2021-11-02 16:09:37 -0600

    Adding suite to test rail add_run call

--- bin/testrail-start-run.js
+++ bin/testrail-start-run.js
@@ -10,16 +10,18 @@ const { getTestRailConfig, getAuthorization } = require('../src/get-config')
 const { findCases } = require('../src/find-cases')
 
 const args = arg(
-  {
-    '--spec': String,
-    '--name': String,
-    '--description': String,
-    // aliases
-    '-s': '--spec',
-    '-n': '--name',
-    '-d': '--description',
-  },
-  { permissive: true },
+	{
+		'--spec': String,
+		'--name': String,
+		'--description': String,
+		'--suite': String,
+		// aliases
+		'-s': '--spec',
+		'-n': '--name',
+		'-d': '--description',
+		'-st': '--suite'
+	},
+	{ permissive: true },
 )
 // optional arguments
 const name = args['--name'] || args._[0]
@@ -29,72 +31,80 @@ debug('run name: %s', name)
 debug('run description: %s', description)
 
 function findSpecs(pattern) {
-  // @ts-ignore
-  return globby(pattern, {
-    absolute: true,
-  })
+	// @ts-ignore
+	return globby(pattern, {
+		absolute: true,
+	})
 }
 
 function startRun({ testRailInfo, name, description, caseIds }) {
-  // only output the run ID to the STDOUT, everything else is logged to the STDERR
-  console.error(
-    'creating new TestRail run for project %s',
-    testRailInfo.projectId,
-  )
-  if (caseIds && caseIds.length > 0) {
-    console.error('With %d case IDs', caseIds.length)
-  }
+	// only output the run ID to the STDOUT, everything else is logged to the STDERR
+	console.error(
+		'creating new TestRail run for project %s',
+		testRailInfo.projectId,
+	)
+	if (caseIds && caseIds.length > 0) {
+		console.error('With %d case IDs', caseIds.length)
+	}
 
-  const addRunUrl = `${testRailInfo.host}/index.php?/api/v2/add_run/${testRailInfo.projectId}`
-  debug('add run url: %s', addRunUrl)
-  const authorization = getAuthorization(testRailInfo)
 
-  const json = {
-    name,
-    description,
-  }
-  if (caseIds && caseIds.length > 0) {
-    json.include_all = false
-    json.case_ids = caseIds
-  }
-  debug('add run params %o', json)
+	const addRunUrl = `${testRailInfo.host}/index.php?/api/v2/add_run/${testRailInfo.projectId}`
+	debug('add run url: %s', addRunUrl)
+	const authorization = getAuthorization(testRailInfo)
 
-  // @ts-ignore
-  return got(addRunUrl, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      authorization,
-    },
-    json,
-  })
-    .json()
-    .then(
-      (json) => {
-        debug('response from the add_run')
-        debug('%o', json)
-        console.log(json.id)
-      },
-      (error) => {
-        console.error(error)
-        process.exit(1)
-      },
-    )
+	const json = {
+		name,
+		description,
+	}
+	if (caseIds && caseIds.length > 0) {
+		json.include_all = false
+		json.case_ids = caseIds
+	}
+
+	const suiteId = args['--suite'] || testRailInfo.suiteId;
+	console.log(args['--suite'])
+	console.log(testRailInfo.suiteId)
+	if (suiteId) {
+		json.suite_id = +suiteId;
+	}
+	debug('add run params %o', json)
+	console.log(JSON.stringify(json));
+	// @ts-ignore
+	return got(addRunUrl, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			authorization,
+		},
+		json,
+	})
+		.json()
+		.then(
+			(json) => {
+				debug('response from the add_run')
+				debug('%o', json)
+				console.log(json.id)
+			},
+			(error) => {
+				console.error(error)
+				process.exit(1)
+			},
+		)
 }
 
 if (args['--spec']) {
-  findSpecs(args['--spec']).then((specs) => {
-    debug('using pattern "%s" found specs', args['--spec'])
-    debug(specs)
-    const caseIds = findCases(specs)
-    debug('found TestRail case ids: %o', caseIds)
+	findSpecs(args['--spec']).then((specs) => {
+		debug('using pattern "%s" found specs', args['--spec'])
+		debug(specs)
+		const caseIds = findCases(specs)
+		debug('found TestRail case ids: %o', caseIds)
 
-    const testRailInfo = getTestRailConfig()
-    startRun({ testRailInfo, name, description, caseIds })
-  })
+		const testRailInfo = getTestRailConfig()
+		startRun({ testRailInfo, name, description, caseIds })
+	})
 } else {
-  const testRailInfo = getTestRailConfig()
-  // start a new test run for all test cases
-  // @ts-ignore
-  startRun({ testRailInfo, name, description })
+	const testRailInfo = getTestRailConfig()
+	// start a new test run for all test cases
+	// @ts-ignore
+	startRun({ testRailInfo, name, description })
 }
--- src/get-config.js
+++ src/get-config.js
@@ -4,69 +4,70 @@ const fs = require('fs')
 const path = require('path')
 
 function hasConfig(env = process.env) {
-  return (
-    'TESTRAIL_HOST' in env ||
-    'TESTRAIL_USERNAME' in env ||
-    'TESTRAIL_PASSWORD' in env ||
-    'TESTRAIL_PROJECTID' in env
-  )
+	return (
+		'TESTRAIL_HOST' in env ||
+		'TESTRAIL_USERNAME' in env ||
+		'TESTRAIL_PASSWORD' in env ||
+		'TESTRAIL_PROJECTID' in env
+	)
 }
 
 function getTestRailConfig(env = process.env) {
-  const debug = require('debug')('cypress-testrail-simple')
+	const debug = require('debug')('cypress-testrail-simple')
 
-  if (!env.TESTRAIL_HOST) {
-    throw new Error('TESTRAIL_HOST is required')
-  }
-  if (!env.TESTRAIL_USERNAME) {
-    throw new Error('TESTRAIL_USERNAME is required')
-  }
-  if (!env.TESTRAIL_PASSWORD) {
-    throw new Error('TESTRAIL_PASSWORD is required. Could be an API key.')
-  }
-  if (!env.TESTRAIL_PROJECTID) {
-    throw new Error('TESTRAIL_PROJECTID is required.')
-  }
+	if (!env.TESTRAIL_HOST) {
+		throw new Error('TESTRAIL_HOST is required')
+	}
+	if (!env.TESTRAIL_USERNAME) {
+		throw new Error('TESTRAIL_USERNAME is required')
+	}
+	if (!env.TESTRAIL_PASSWORD) {
+		throw new Error('TESTRAIL_PASSWORD is required. Could be an API key.')
+	}
+	if (!env.TESTRAIL_PROJECTID) {
+		throw new Error('TESTRAIL_PROJECTID is required.')
+	}
 
-  const testRailInfo = {
-    host: process.env.TESTRAIL_HOST,
-    username: process.env.TESTRAIL_USERNAME,
-    password: process.env.TESTRAIL_PASSWORD,
-    projectId: process.env.TESTRAIL_PROJECTID,
-  }
-  debug('test rail info without the password')
-  debug('%o', { ...testRailInfo, password: '***' })
+	const testRailInfo = {
+		host: process.env.TESTRAIL_HOST,
+		username: process.env.TESTRAIL_USERNAME,
+		password: process.env.TESTRAIL_PASSWORD,
+		projectId: process.env.TESTRAIL_PROJECTID,
+		suiteId: process.env.TESTRAIL_SUITEID
+	}
+	debug('test rail info without the password')
+	debug('%o', { ...testRailInfo, password: '***' })
 
-  return testRailInfo
+	return testRailInfo
 }
 
 function getAuthorization(testRailInfo) {
-  const authorization = `Basic ${Buffer.from(
-    `${testRailInfo.username}:${testRailInfo.password}`,
-  ).toString('base64')}`
-  return authorization
+	const authorization = `Basic ${Buffer.from(
+		`${testRailInfo.username}:${testRailInfo.password}`,
+	).toString('base64')}`
+	return authorization
 }
 
 function getTestRunId(env = process.env) {
-  // first, try to read the test run id from the environment
-  if ('TESTRAIL_RUN_ID' in env) {
-    return parseInt(env.TESTRAIL_RUN_ID)
-  }
+	// first, try to read the test run id from the environment
+	if ('TESTRAIL_RUN_ID' in env) {
+		return parseInt(env.TESTRAIL_RUN_ID)
+	}
 
-  const filename = path.join(process.cwd(), 'runId.txt')
-  debug('checking file %s', filename)
+	const filename = path.join(process.cwd(), 'runId.txt')
+	debug('checking file %s', filename)
 
-  if (fs.existsSync(filename)) {
-    const s = fs.readFileSync(filename, 'utf8').trim()
-    console.log('read "%s"', s)
-    return parseInt(s)
-  }
-  debug('could not find runId.txt in folder %s', process.cwd())
+	if (fs.existsSync(filename)) {
+		const s = fs.readFileSync(filename, 'utf8').trim()
+		console.log('read "%s"', s)
+		return parseInt(s)
+	}
+	debug('could not find runId.txt in folder %s', process.cwd())
 }
 
 module.exports = {
-  hasConfig,
-  getTestRailConfig,
-  getAuthorization,
-  getTestRunId,
+	hasConfig,
+	getTestRailConfig,
+	getAuthorization,
+	getTestRunId,
 }
